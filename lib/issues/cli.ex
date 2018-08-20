e8e911c100c563998f3f10166adfef0d6fe3a18d
Yifeng Wang <yifeng@yeefom.com>
2018-08-20 08:34:33 -0700

    process args

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -2,13 +2,24 @@ defmodule Issues.Cli do
   @default_count 4
 
   def run(argv) do
-   parse_args(argv)
+    argv
+    |> parse_args()
+    |> process
   end
 
   def parse_args(argv) do
     OptionParser.parse(argv, switches: [ help: :boolean ], aliases: [ h: :help ])
     |> elem(1)
     |> _parse_args
+  end
+
+  def process(:help) do
+    IO.puts("Usage: issues <user> <project> [ count | #{@default_count} ]")
+    System.halt(0)
+  end
+
+  def process({ user, project, count }) do
+    # fetch github issues
   end
 
   defp _parse_args([ user, project, count ]), do: { user, project, String.to_integer(count) }
