fd884841b5e78711fd479212ca035d13b2686853
Yifeng Wang <yifeng@yeefom.com>
2018-08-19 20:14:49 -0700

    clean up code

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -6,11 +6,12 @@ defmodule Issues.Cli do
   end
 
   def parse_args(argv) do
-    parsed = OptionParser.parse(argv, switches: [ help: :boolean ], aliases: [ h: :help ])
-    case parsed do
-      { [ help: true ], _, _ } -> :help
-      { _, [ user, project, count ], _} -> { user, project, String.to_integer(count) }
-      { _, [ user, project], _} -> { user, project, @default_count }
-    end
+    OptionParser.parse(argv, switches: [ help: :boolean ], aliases: [ h: :help ])
+    |> elem(1)
+    |> _parse_args
   end
+
+  defp _parse_args([ user, project, count ]), do: { user, project, String.to_integer(count) }
+  defp _parse_args([ user, project ]), do: { user, project, @default_count }
+  defp _parse_args(_), do: :help
 end
