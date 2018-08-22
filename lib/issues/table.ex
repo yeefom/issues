cf2b66e002c61a253728f27c3f981668608f98df
Yifeng Wang <yifeng@yeefom.com>
2018-08-21 19:05:39 -0700

    create table

--- lib/issues/table.ex
+++ lib/issues/table.ex
@@ -1,7 +1,7 @@
 defmodule Issues.Table do
   def create(entries) do
     find_length(entries)
-    |> pad_entries(entries)
+    |> create_rows(entries)
   end
 
   def find_length(entries) do
@@ -14,6 +14,20 @@ defmodule Issues.Table do
     end)
   end
 
-  def pad_entries(lengthes, entries) do
+  def create_rows(lengthes, entries) do
+    [ head | tail ] = Enum.map(entries, fn entry -> create_row(entry, lengthes) end)
+    divider = creat_divider(lengthes)
+    [ head | [ divider | tail ] ]
+  end
+
+  def create_row(entry, lengthes) do
+    Enum.zip(entry, lengthes)
+    |> Enum.map(fn { value, length } -> String.pad_trailing(value, length) end)
+    |> Enum.join(" | ")
+  end
+
+  def creat_divider(lengthes) do
+    Enum.map(lengthes, fn length -> String.duplicate("-", length) end)
+    |> Enum.join("-+-")
   end
 end
