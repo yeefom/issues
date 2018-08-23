8eed248064b369e872e6afd24f40fd75f4a033c1
Yifeng Wang <yifeng@yeefom.com>
2018-08-23 08:17:45 -0700

    add a logger

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [ { "User-agent", "issues" } ]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issue_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
